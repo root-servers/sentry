11294aa5b48b5e133dd252e8c95efb15c593a7ce
Alberto Leal <mail4alberto@gmail.com>
2020-05-13 00:36:51 -0400

    span title

--- src/sentry/static/sentry/app/components/events/interfaces/spans/spanBar.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/spanBar.tsx
@@ -146,7 +146,7 @@ const INTERSECTION_THRESHOLDS: Array<number> = [
 
 const TOGGLE_BUTTON_MARGIN_RIGHT = 16;
 const TOGGLE_BUTTON_MAX_WIDTH = 30;
-const TOGGLE_BORDER_BOX = TOGGLE_BUTTON_MAX_WIDTH + TOGGLE_BUTTON_MARGIN_RIGHT;
+export const TOGGLE_BORDER_BOX = TOGGLE_BUTTON_MAX_WIDTH + TOGGLE_BUTTON_MARGIN_RIGHT;
 const MARGIN_LEFT = 0;
 
 type DurationDisplay = 'left' | 'right' | 'inset';
@@ -930,7 +930,7 @@ const DividerLineGhostContainer = styled('div')`
   height: 100%;
 `;
 
-const SpanBarTitleContainer = styled('div')`
+export const SpanBarTitleContainer = styled('div')`
   display: flex;
   align-items: center;
   height: 100%;
@@ -941,7 +941,7 @@ const SpanBarTitleContainer = styled('div')`
   user-select: none;
 `;
 
-const SpanBarTitle = styled('div')`
+export const SpanBarTitle = styled('div')`
   position: relative;
   height: 100%;
   font-size: ${p => p.theme.fontSizeSmall};
@@ -1130,7 +1130,7 @@ const StyledIconChevron = styled(IconChevron)`
   margin-left: ${space(0.25)};
 `;
 
-const OperationName = styled('span')<{spanErrors: TableDataRow[]}>`
+export const OperationName = styled('span')<{spanErrors: TableDataRow[]}>`
   color: ${p => (p.spanErrors.length ? p.theme.error : 'inherit')};
 `;
 
